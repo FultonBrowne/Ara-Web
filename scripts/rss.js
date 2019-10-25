8cfdf24c2a2a40062a0c108333c21e914e307871
swarupe <swarup.e1998@gmail.com>
2019-10-25 23:33:07 +0530

    variable created for url

--- scripts/rss.js
+++ scripts/rss.js
@@ -1,6 +1,7 @@
+var feedUrl = 'https://araserver.herokuapp.com';
 $.ajax({
     type: 'GET',
-    url: "https://api.rss2json.com/v1/api.json?rss_url=" + 'https://araserver.herokuapp.com',
+    url: "https://api.rss2json.com/v1/api.json?rss_url=" + feedUrl,
     dataType: 'jsonp',
     success: function(result) {
         console.log(result);
